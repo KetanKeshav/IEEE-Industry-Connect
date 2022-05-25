6c8313de6c16a01ad9d37c3412f2ce283f7bfa75
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-05-25 15:46:53 +0530

    photos

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
           <li><a class="nav-link scrollto" href="#about">About The Event</a></li>
           <li><a class="nav-link scrollto" href="#pricing">Sponsorships</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">Agenda</a></li>
-          <li><a class="nav-link scrollto" href="#team">Committee</a></li>
+          <li><a class="nav-link scrollto" href="#team">Committees</a></li>
           <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
