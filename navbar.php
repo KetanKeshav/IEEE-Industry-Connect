37732578d7de8187ae0f7f98d44f1b8056af94e3
anzar00 <peerzada.anzar@gmail.com>
2022-05-25 16:48:21 +0530

    navbar

--- navbar.php
+++ navbar.php
@@ -15,25 +15,8 @@
           <li><a class="nav-link scrollto" href="#pricing">Sponsorships</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">Agenda</a></li>
           <li><a class="nav-link scrollto" href="./committees/">Committee</a></li>
-          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> -->
+          <li><a class="nav-link scrollto" href="#student-reg">Registration</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Registration</a></li>
           <li><a class="getstarted scrollto" href="https://in.explara.com/e/ypsic2022">Register Now</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
