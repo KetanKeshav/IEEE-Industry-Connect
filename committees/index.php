5ee24e20e74a060de979c0990b19ba9f5030bbcd
anzar00 <peerzada.anzar@gmail.com>
2022-05-25 16:13:08 +0530

    oc

--- committees/index.php
+++ committees/index.php
@@ -187,9 +187,9 @@
         <div class="row">
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="member" data-aos="fade-up">
-              <div class="pic"><img src="../assets/img/team/team-1.jpg" alt=""></div>
-              <h4>Walter White</h4>
-              <span>Chief Executive Officer</span>
+              <div class="pic"><img src="../assets/img/team/shazib.jpg" alt=""></div>
+              <h4>Shazib Javed Wani</h4>
+              <span>Volunteer</span>
               <div class="social">
                 <a href=""><i class="bi bi-twitter"></i></a>
                 <a href=""><i class="bi bi-facebook"></i></a>
@@ -201,9 +201,9 @@
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="member" data-aos="fade-up" data-aos-delay="100">
-              <div class="pic"><img src="../assets/img/team/team-2.jpg" alt=""></div>
-              <h4>Sarah Jhinson</h4>
-              <span>Product Manager</span>
+              <div class="pic"><img src="../assets/img/team/soumya.jpg" alt=""></div>
+              <h4>Soumyadip Roy</h4>
+              <span>Volunteer</span>
               <div class="social">
                 <a href=""><i class="bi bi-twitter"></i></a>
                 <a href=""><i class="bi bi-facebook"></i></a>
@@ -215,23 +215,9 @@
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="pic"><img src="../assets/img/team/team-3.jpg" alt=""></div>
-              <h4>William Anderson</h4>
-              <span>CTO</span>
-              <div class="social">
-                <a href=""><i class="bi bi-twitter"></i></a>
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
-              <div class="pic"><img src="../assets/img/team/team-4.jpg" alt=""></div>
-              <h4>Amanda Jepson</h4>
-              <span>Accountant</span>
+              <div class="pic"><img src="../assets/img/team/anzar.jpg" alt=""></div>
+              <h4>Peerzada Anzar Azmat</h4>
+              <span>Volunteer</span>
               <div class="social">
                 <a href=""><i class="bi bi-twitter"></i></a>
                 <a href=""><i class="bi bi-facebook"></i></a>
