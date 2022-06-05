b5e9cde0d0b1c4ae2c1c23f88e16cd27e1efebfc
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-06-05 19:39:04 +0530

    speakerspagefina;

--- speakers/index.php
+++ speakers/index.php
@@ -62,7 +62,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal1Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal1" tabindex="-1" aria-labelledby="Modal1Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -75,7 +75,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/avisek.jpg" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dr. Avisek Lahiri, Ph.D. is currently a Research Software Engineer at Google. He holds a Ph.D. from IIT Kharagpur on generative deep learning and was a Google Ph.D. Fellow. He has been a recipient of the HLF Young Researcher and the Qualcomm Innovation Fellowship twice. He is a computer vision enthusiast and who was also part of an industrial research project funded by Indian Space Research Organization. He has also pursued research internships from Facebook and Twitter.</div>
                     </div>
                   </div>
                 </div>
@@ -102,7 +102,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal2Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal2" tabindex="-1" aria-labelledby="Modal2Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -115,7 +115,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/jyotirmoy.jpg" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dr. Jyotirmoy Banerjee, Ph.D. is currently a Principal Data Scientist with Ericsson. He did his postdoctoral research from UCL, London, UK after completing his Ph.D. from Erasmus MC, Rotterdam, Netherlands. His areas of interest include machine learning, computer vision and natural language processing, and has experience in telecom, ecommerce and healthcare domains. He has previously worked with Amazon, Flipkart and GE. </div>
                     </div>
                   </div>
                 </div>
@@ -142,7 +142,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal3Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal3" tabindex="-1" aria-labelledby="Modal3Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -155,7 +155,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/dhaval.png" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dhaval Patel is a co-founder at AtliQ Technologies. He is a passionate data and software services entrepreneur. Prior to AtliQ, he worked for Bloomberg and NVIDIA. He is also passionate about ed-tech and teaches data science and machine learning on his YouTube channel codebasics. </div>
                     </div>
                   </div>
                 </div>
@@ -182,7 +182,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal4" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal4Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal4" tabindex="-1" aria-labelledby="Modal4Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -195,7 +195,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/himanshu.jpg" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dr. Himanshu Pant is currently the head of analytics at Adani Green. Dr Pant holds a Ph.D. in machine learning from IIT Delhi. Prior to Adani, Dr. Pant has worked with National Thermal Power Corporation (NTPC) in various capacities, most recently as Deputy General Manager. Dr Pant's research interests include deep learning, data analytics and its applications in the power and energy sectors.</div>
                     </div>
                   </div>
                 </div>
@@ -222,7 +222,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal5" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal5Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal5" tabindex="-1" aria-labelledby="Modal5Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -235,7 +235,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/badrinath.jpg" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dr. Badrinath Ramamurthy joined IIIT-Bangalore from Ericsson Research, Bangalore where he was working between 2014 and 2021. He was part of a team that deals with the machine reasoning technologies in AI. This includes technologies such as AI planning and Semantic web. His areas of application have been zero-touch automation and IoT. Prior to Ericsson he was with HP, Bangalore, between 2003 and 2014, where he worked in the areas of Cloud Systems and High Performance Computing. Prior to joining the Computer Industry in he was a faculty member at IIT Kharagpur between 1994 and 2003. He was also a visiting researcher at IRISIA, Rennes during 2002-2003. His core areas of research there have been in Graph Theory and Interconnection Networks. Badrinath has a Masters and PhD from Rensselaer Polytechnic Institute, NY USA (1994) and a MSc(Tech) from BITS, Pilani (1987), all in Computer Science.</div>
                     </div>
                   </div>
                 </div>
@@ -262,7 +262,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal6" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal6Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal6" tabindex="-1" aria-labelledby="Modal6Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -275,7 +275,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/manasa.jpg" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Dr Manasa K is currently an Associate Director for the Creativity AI at Games24X7. She holds a Ph.D. in Communication Signal Processing from Indian Institute of Technology, Hyderabad. She has experience in working with real-world problems in the domains of Computer Vision, Image/Video Analytics, Deep Learning and Biomedical Image Processing. Prior to Games 24X7, Dr Manasa K has worked with Conduent Labs (formerly known as Xerox Research Center India) and SONY.</div>
                     </div>
                   </div>
                 </div>
@@ -302,7 +302,7 @@
           </div>
 
           <!-- Modal -->
-          <div class="modal fade bd-example-modal-lg" id="Modal7" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="Modal7Label" aria-hidden="true">
+          <div class="modal fade bd-example-modal-lg" id="Modal7" tabindex="-1" aria-labelledby="Modal7Label" aria-hidden="true">
             <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -315,7 +315,7 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-md-4"><img src="../assets/img/Speakers/" style="max-width: 200px;" alt=""></div>
-                      <div class="col-md-4 ml-auto">Info</div>
+                      <div class="col-md-8 ml-auto">Chhavi Chawla is an Advanced Data Scientist with Honeywell. She has a demonstrated history of working in the data science industry. and is a strong research professional with a PGDBDA focused in Data Analytics from Centre for Development and Advanced Computing, Bengaluru. Prior to Honeywell, Chhavi has worked with Bosch Centre for Artificial Intelligence as well as a research intern at the Indian Institute of Science, Bangalore.</div>
                     </div>
                   </div>
                 </div>
