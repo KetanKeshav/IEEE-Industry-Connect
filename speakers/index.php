3b2a0b4e7435fbf0055c43e8a470ac969485c5d8
anzar00 <peerzada.anzar@gmail.com>
2022-06-05 17:45:06 +0530

    speakers page

--- speakers/index.php
+++ speakers/index.php
@@ -44,15 +44,15 @@
         </div>
 
         <div class="row">
+          
+          <!-- Speaker -->
+
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="member" data-aos="fade-up">
               <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
               <h4>Depdeep Sarkar</h4>
               <span>Chair</span>
               <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
                   Know More
@@ -82,112 +82,250 @@
               </div>
             </div>
           </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
           
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="100">
-              <div class="pic"><img src="../assets/img/team/kishore.jpg" alt=""></div>
-              <h4>Kishore Kumar Sharma</h4>
-              <span>Chair Elect</span>
-              <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="pic"><img src="../assets/img/team/Neha.jpg" alt=""></div>
-              <h4>Neha Kumari</h4>
-              <span>Vice Chair</span>
-              <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
-              <div class="pic"><img src="../assets/img/team/Samarth.jpg" alt=""></div>
-              <h4>Samarth Bonthala</h4>
-              <span>Vice Chair</span>
-              <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
-              <div class="pic"><img src="../assets/img/team//Ketan.jpg" alt=""></div>
-              <h4>Ketan Keshav</h4>
-              <span>Treasurer</span>
-              <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="pic"><img src="../assets/img/team/sumit.jpg" alt=""></div>
-              <h4>Dr. Sumit Soman</h4>
-              <span>Execom Member</span>
-              <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="member" data-aos="fade-up">
-              <div class="pic"><img src="../assets/img/team/nagalekha.jpg" alt=""></div>
-              <h4>Nagalekha Ramesh</h4>
-              <span>Execom Member</span>
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
               <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
               </div>
             </div>
           </div>
 
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
+          
           <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up" data-aos-delay="100">
-              <div class="pic"><img src="../assets/img/team/pramod.jpg" alt=""></div>
-              <h4>Pramood BN</h4>
-              <span>Execom Member</span>
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
               <div class="social">
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-xl-3 col-lg-4 col-md-6">
-          <div class="member" data-aos="fade-up" data-aos-delay="100">
-            <div class="pic"><img src="../assets/img/team/skanda.jpg" alt=""></div>
-            <h4>Skanda Prasad</h4>
-            <span>Execom Member</span>
-            <div class="social">
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
+          
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
+              <div class="social">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
+          
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
+              <div class="social">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
+          
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
+              <div class="social">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End Speaker -->
+
+          <!-- Speaker -->
+          
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div class="member" data-aos="fade-up">
+              <div class="pic"><img src="../assets/img/team/debdeep.jpg" alt=""></div>
+              <h4>Depdeep Sarkar</h4>
+              <span>Chair</span>
+              <div class="social">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+                  Know More
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <!-- Modal -->
+          <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4"><img src="../assets/img/team/debdeep.jpg" style="max-width: 200px;" alt=""></div>
+                      <div class="col-md-4 ml-auto">Info</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End Speaker -->
+
+          
       </div>
     </section><!-- End Execom Section -->
 
